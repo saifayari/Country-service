9a685e67dd1de6520d5e54b0a48a6261e07c0025
Saif_ayari <saif.ayari.1717@gmail.com>
2026-04-16 17:11:14 +0100

    add docker setup and project configuration

--- country-service/src/main/java/com/example/countryservice/beans/Country.java
+++ country-service/src/main/java/com/example/countryservice/beans/Country.java
@@ -1,26 +1,37 @@
 package com.example.countryservice.beans;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 @Table(name="country")
-
 public class Country {
-	@Id
-	@Column(name="id")
-	private int idCountry;
-	@Column(name="name_country")
-	private String name;
-	@Column(name="capital_name")
-	private String Capital;
 
+    @Id
+    @Column(name="id")
+    private int idCountry;
 
+    @Column(name="name_country")
+    private String name;
+
+    @Column(name="capital_name")
+    private String capital;
+
+    public Country() {}
+
+    public Country(int idCountry, String name, String capital) {
+        this.idCountry = idCountry;
+        this.name = name;
+        this.capital = capital;
+    }
+
+    public int getIdCountry() { return idCountry; }
+    public void setIdCountry(int idCountry) { this.idCountry = idCountry; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getCapital() { return capital; }
+    public void setCapital(String capital) { this.capital = capital; }
 }
